8d1e601cb2b6f75a8e0214d2cdc355f8fd71e97a
bepzi <dev.ben01189998819991197253@gmail.com>
2019-05-15 10:15:25 -0500

    Return by value, not by unique_ptr
    C++ will do automatic copy-elision, and the vector's data is already
    stored on the heap anyways so the copy penalty would be small.

--- src/util.hpp
+++ src/util.hpp
@@ -11,21 +11,22 @@ using std::array;
 using std::unique_ptr;
 using std::vector;
 
-unique_ptr<vector<char>> read_file(const std::string &filename) {
+vector<char> read_file(const std::string &filename) {
+    // File will be opened at the end so that we can get the size
     std::ifstream file(filename, std::ios::ate | std::ios::binary);
-
     if (!file.is_open()) {
         throw std::runtime_error("failed to open file!");
     }
 
-    std::streamsize file_size = file.tellg();
-    auto buffer = std::make_unique<vector<char>>(file_size + 1);
+    std::streamoff file_size = file.tellg();
+    auto buffer = vector<char>((size_t)file_size + 1);
 
     file.seekg(0);
-    file.read(buffer->data(), file_size);
+    file.read(buffer.data(), file_size);
     file.close();
 
-    buffer->push_back('\0');
+    // Manually null-terminate the contents
+    buffer.push_back('\0');
 
     return buffer;
 }
@@ -34,7 +35,7 @@ GLuint compile_shader(const std::string &filename, GLenum shader_type) {
     GLuint shader = glCreateShader(shader_type);
 
     const auto source_vec = read_file(filename);
-    const GLchar *shader_sources[] = {source_vec->data()};
+    const GLchar *shader_sources[] = {source_vec.data()};
 
     glShaderSource(shader, 1, shader_sources, nullptr);
     glCompileShader(shader);
@@ -61,11 +62,9 @@ GLuint compile_shader(const std::string &filename, GLenum shader_type) {
 
 GLuint link_program(const std::vector<GLuint> &shaders) {
     GLuint program = glCreateProgram();
-
     for (GLuint shader : shaders) {
         glAttachShader(program, shader);
     }
-
     glLinkProgram(program);
 
     GLint success = 0;
